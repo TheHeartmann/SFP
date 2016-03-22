028b0b6a8eda7f66215e953d4eee5927ddb1e1ef
Thomas <Thomas@Pia-MAC.local>
2016-03-22 13:38:08 +0100

    JS now works. Only implemented for cb

--- js/sfp_custom.js
+++ js/sfp_custom.js
@@ -1,17 +1,25 @@
+// Get the modal
 var modal = document.getElementById('cbModal');
-var btn = document.getElementById("cbBtn");
+
+// Get the button that opens the modal
+var btn = document.getElementById("box2");
+
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closeX")[0];
 
-btn.onclick = function(){
-	modal.style.display="block";
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-span.onclick = function(){
-	modal.style.display="none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
-window.onclick = function(){
-	if (event.target == modal){
-		modal.style.display="none";
-	}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
