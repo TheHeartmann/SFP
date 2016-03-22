ff9cc326f5c00a155a8537dcff595bee26d339e8
Thomas <Thomas@Pia-MAC.local>
2016-03-22 12:42:37 +0100

    JS not working

--- js/sfp_custom.js
+++ js/sfp_custom.js
@@ -1,1 +1,17 @@
-SfP
+var modal = document.getElementById('cbModal');
+var btn = document.getElementById("cbBtn");
+var span = document.getElementsByClassName("closeX")[0];
+
+btn.onclick = function(){
+	modal.style.display="block";
+}
+
+span.onclick = function(){
+	modal.style.display="none";
+}
+
+window.onclick = function(){
+	if (event.target == modal){
+		modal.style.display="none";
+	}
+}
