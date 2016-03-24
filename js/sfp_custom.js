3a6acd49d96c56d09103bdb56faab77476961e10
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2016-03-24 15:54:05 +0100

    Added JS for all boxes. Need to find a way to compress code. PHP?

--- js/sfp_custom.js
+++ js/sfp_custom.js
@@ -1,3 +1,4 @@
+/*
 // Get the modal
 var modal = document.getElementById('cbModal');
 
@@ -8,18 +9,49 @@ var btn = document.getElementById("box2");
 var span = document.getElementsByClassName("closeX")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+btn.onclick = function () {
+	modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+span.onclick = function () {
+	modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+*/
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeX")[0];
+
+
+function openModal(game) {
+
+	var modal;
+
+	if (game == 'cb') {
+		var modal = document.getElementById('cbModal');
+	}else if (game == 'tr') {
+		var modal = document.getElementById('trModal');
+	}else if (game == 'cm') {
+		var modal = document.getElementById('cmModal');
+	}else if (game == 'sfp') {
+		var modal = document.getElementById('showreelModal');
+	}
+
+	modal.style.display = "block";
+	span.onclick = function () {
+		modal.style.display = "none";
+	}
+
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
 }
